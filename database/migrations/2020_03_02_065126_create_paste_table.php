84e19afcb6def2056b29dd0a53dd76065d87a021
getof <infogetof@gmail.com>
2020-03-02 14:04:06 +0700

    Edit BD

---
+++ database/migrations/2020_03_02_065126_create_paste_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePasteTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('paste', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title', 50);
+            $table->text('paste');
+            $table->string('sintax', 50);
+            $table->integer('expir');
+            $table->boolean('expose');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('paste');
+    }
+}
